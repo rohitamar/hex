8d1ea1482ec7cbfbb17165abf74056195291c335
rohitamar <rohitamar@gmail.com>
2025-03-25 15:31:14 -0400

    ??

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, jsonify, request
 from flask_cors import CORS
 import random 
+import ast 
 
-app = Flask(__name__, static_folder='../frontend')  # Point to frontend folder directly
+app = Flask(__name__, static_folder='../frontend') 
 CORS(app)
+
+def parse_grid_string(grid_string):
+    grid_string = grid_string.replace('W', '"W"').replace('B', '"B"').replace('R', '"R"')
+    return ast.literal_eval(grid_string)
 
 @app.route('/')
 def serve_index():
@@ -15,10 +20,16 @@ def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 @app.route('/random', methods=['GET'])
-def hello():
+def random_play():
+    grid_string = request.args.get('grid', default='none')
+    grid = parse_grid_string(grid_string)
+    coords = [[i, j] for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == "W"]
+    rand_coord = random.choice(coords)
+
     return jsonify({
-        'pair': [random.randint(0, 11), random.randint(0, 11)]
+        'pair': rand_coord,
+        'endgame': True
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)  # Debug mode for local dev
+    app.run(host='0.0.0.0', port=5000)  # Debug mode for local dev
